2989c3b6026a9916236c758be44978680e6325ea
Xin3851 <zxx3851@foxmial.com>
2019-12-23 11:59:30 +0800

    合并分支

---
+++ vuepctext/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+//  view UI
+import ViewUI from '../node_modules/view-design';
+import 'view-design/dist/styles/iview.css';
+Vue.use(ViewUI);
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
